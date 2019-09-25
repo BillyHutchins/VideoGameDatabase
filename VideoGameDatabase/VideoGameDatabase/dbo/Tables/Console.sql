86a0587ee1032ba4d84d2821df49c83c4f4f691b
Billy Hutchins <bhutchins@spscommerce.com>
2019-09-25 15:57:38 -0400

    Video Game database initial commit

---
+++ VideoGameDatabase/VideoGameDatabase/dbo/Tables/Console.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Console] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Name]    NVARCHAR (200) NULL,
+    [Company] NVARCHAR (200) NULL,
+    [Year]    NVARCHAR (10)  NULL,
+    [Units]   INT            NULL,
+    CONSTRAINT [PK_Console] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ VideoGameDatabase/VideoGameDatabase/dbo/Tables/Game.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Game] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (200) NULL,
+    [ConsoleId] INT            NULL,
+    [Year]      NVARCHAR (10)  NULL,
+    [Units]     INT            NULL,
+    CONSTRAINT [PK_Game] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Game_Console] FOREIGN KEY ([ConsoleId]) REFERENCES [dbo].[Console] ([Id])
+);
+
